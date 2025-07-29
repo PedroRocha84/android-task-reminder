75174cde73506e030871bc399719bd6a16ab8efa
PedroRocha84 <rocha.pedroandre@outlook.com>
2025-07-29 15:04:36 +0100

    first comit

---
+++ app/src/main/java/pt/pedrorocha/android/taskmanager/MainActivity.java
@@ -0,0 +1,26 @@
+package pt.pedrorocha.android.taskmanager;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button btnNewTask = findViewById(R.id.button);
+        btnNewTask.setOnClickListener(v->{
+
+        });
+
+    }
+}

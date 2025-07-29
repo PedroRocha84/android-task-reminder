8ef40cedbe635d4dd82e64b3db31d8f116eaf1b8
PedroRocha84 <rocha.pedroandre@outlook.com>
2025-07-29 17:01:21 +0100

    RecyclerView working with dummy data, Card Layout improved also

---
+++ app/src/main/java/pt/pedrorocha/android/taskmanager/Task.java
@@ -0,0 +1,37 @@
+package pt.pedrorocha.android.taskmanager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Task {
+
+    private int id;
+    private String title;
+    private String description;
+
+    public Task(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static List<Task> list(){
+        Task task1 = new Task(0,"My first task", "A little description for testing my app");
+        Task task2 = new Task(1,"My second task", "A little description for testing my app");
+
+        return new ArrayList<>(Arrays.asList(task1,task2));
+    }
+}
